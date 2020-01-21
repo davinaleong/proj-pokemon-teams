47a78edddd4f451eac8eac50537257c90359a1d2
Davina Leong <davina.leong@outlook.com>
2020-01-21 16:35:23 +0800

    Added site JSON data file

---
+++ data/site.js
@@ -0,0 +1,19 @@
+const site = {
+    "title": "Pokemon Teams",
+    "copyright": {
+        "name": "Pokemon Teams",
+        "author": "Davina Leong",
+        "year": "2020"
+    },
+    "assets": {
+        "icon": "./icons/",
+        "image": "./images/",
+        "pokemon": {
+            "items": "./images/items/",
+            "sprites": "./images/pokemon/",
+            "icons": "./images/pokemon/icons/"
+        }
+    }
+};
+
+export default site;
