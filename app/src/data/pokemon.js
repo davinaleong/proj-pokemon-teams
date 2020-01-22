8b82cbbce0671b580ea9250a1d2ccc08182f3016
Davina Leong <davina.leong@outlook.com>
2020-01-22 14:53:17 +0800

    Gave keys to images

--- app/src/data/pokemon.js
+++ app/src/data/pokemon.js
@@ -2,167 +2,166 @@ const pokemon = [
     {
         "name": "Aerodactyl",
         "types": ["Rock", "Flying"],
-        "images": [
-            "aerodactyl.gif",
-            "aerodactyl.png"
-        ],
-        "icon": "aerodactyl-icon.png"
+        "images": {
+            "animated": "aerodactyl.gif",
+            "sprite": "aerodactyl.png",
+            "icon": "aerodactyl-icon.png"
+        }
     }, {
         "name": "Pikachu",
         "types": ["Electric"],
-        "image": [
-            "pikachu.gif",
-            "pikachu.png"
-        ],
-        "icon": "pikachu-icon.png"
+        "images": {
+            "animated": "pikachu.gif",
+            "sprite": "pikachu.png",
+            "icon": "pikachu-icon.png"
+        }
     }, {
         "name": "Venusaur",
         "types": ["Grass", "Poison"],
-        "image": [
-            "venusaur.gif",
-            "venusaur.png"
-        ],
-        "icon": "venusaur-icon.png"
+        "images": {
+            "animated": "venusaur.gif",
+            "sprite": "venusaur.png",
+            "icon": "venusaur-icon.png"
+        }
     }, {
         "name": "Blastoise",
         "types": ["Water"],
-        "image": [
-            "blastoise.gif",
-            "blastoise.png"
-        ],
-        "icon": "blastoise-icon.png"
+        "images": {
+            "animated": "blastoise.gif",
+            "sprite": "blastoise.png",
+            "icon": "blastoise-icon.png"
+        }
     }, {
         "name": "Charizard",
         "types": ["Fire", "Flying"],
-        "images": [
-            "charizard.gif",
-            "charizard.png"
-        ],
-        "icon": [
-            "charizard-icon.png"
-        ],
+        "images": {
+            "animated": "charizard.gif",
+            "sprite": "charizard.png",
+            "icon": "charizard-icon.png",
+        },
         "megas": [
             {
                 "name": "X",
                 "types": ["Fire", "Dragon"],
-                "images": [
-                    "charizard_mega_x.gif",
-                    "charizard_mega_x.png"
-                ]
+                "images": {
+                    "animated": "charizard_mega_x.gif",
+                    "sprite": "charizard_mega_x.png"
+                }
             }, {
                 "name": "Y",
                 "types": ["Fire", "Flying"],
-                "images": [
-                    "charizard_mega_y.gif",
-                    "charizard_mega_y_.png"
-                ]
+                "images": {
+                    "animated": "charizard_mega_y.gif",
+                    "sprite": "charizard_mega_y_.png"
+                }
             }
         ]
     }, {
         "name": "Clefable",
         "types": ["Fairy"],
-        "images": [
-            "clefable.gif",
-            "clefable.png"
-        ]
+        "images": {
+            "animated": "clefable.gif",
+            "sprite": "clefable.png",
+            "icon": "clefable-icon.png"
+        }
     }, {
         "name": "Raichu",
         "types": ["Electric"],
-        "images": [
-            "raichu.gif",
-            "raichu.png"
-        ],
-        "icon": "raichu-icon.png",
+        "images": {
+            "animated": "raichu.gif",
+            "sprite": "raichu.png",
+            "icon": "raichu-icon.png",
+        }
     }, {
         "name": "Raichu (Alolan)",
         "types": ["Electric", "Psychic"],
-        "images": [
-            "raichu_alola.gif",
-            "raichu_alola.png"
-        ],
-        "icon": "raichu_alola.png"
+        "images": {
+            "animated": "raichu_alola.gif",
+            "sprite": "raichu_alola.png",
+            "icon": "raichu_alola.png"
+        },
     }, {
         "name": "Primarina",
         "types": ["Water", "Fairy"],
-        "images": [
-            "primarina.gif",
-            "primarina.png"
-        ],
-        "icon": "primarina.png"
+        "images": {
+            "animated": "primarina.gif",
+            "sprite": "primarina.png",
+            "icon": "primarina.png"
+        }
     }, {
         "name": "Muk (Alolan)",
         "types": ["Poison", "Dark"],
-        "images": [
-            "muk_alola.gif",
-            "muk_alola.png"
-        ],
-        "icon": "muk-icon.png"
+        "images": {
+            "animated": "muk_alola.gif",
+            "sprite": "muk_alola.png",
+            "icon": "muk-icon.png"
+        }
     }, {
         "name": "Garchomp",
         "types": ["Dragon", "Ground"],
-        "images": [
-            "garchomp.gif",
-            "garchomp.png"
-        ],
-        "icon": "garchomp-icon.png"
+        "images": {
+            "animated": "garchomp.gif",
+            "sprite": "garchomp.png",
+            "icon": "garchomp-icon.png"
+        }
     }, {
         "name": "Magmortar",
         "types": ["Fire"],
-        "images": [
-            "magmortar.gif",
-            "magmortar.png"
-        ],
-        "icon": "magmortar-icon.png"
+        "images": {
+            "animated": "magmortar.gif",
+            "sprite": "magmortar.png",
+            "icon": "magmortar-icon.png"
+        }
     }, {
         "name": "Lucario",
         "types": ["Fighting", "Steel"],
-        "images": [
-            "lucario.gif",
-            "lucario.png"
-        ],
-        "icon": "lucario-icon.png",
+        "images": {
+            "animated": "lucario.gif",
+            "sprite": "lucario.png",
+            "icon": "lucario-icon.png",
+        },
         "megas": [
             {
                 "name": "mega",
                 "types": ["Fighting", "Steel"],
-                "images": [
-                    "lucario_mega.gif",
-                    "lucario_mega.png"
-                ]
+                "images": {
+                    "animated": "lucario_mega.gif",
+                    "sprite": "lucario_mega.png"
+                }
             }
         ]
     }, {
         "name": "Archeops",
         "types": ["Rock", "Flying"],
-        "images": [
-            "archeops.gif",
-            "archeops.png"
-        ],
-        "icon": "archeops-icon.png"
+        "images": {
+            "animated": "archeops.gif",
+            "sprite": "archeops.png",
+            "icon": "archeops-icon.png"
+        }
     }, {
         "name": "Magnezone",
         "types": ["Electric", "Steel"],
-        "images": [
-            "magnezone.gif",
-            "magnezone.png"
-        ],
-        "icon": "magnezone-icon.png"
+        "images": {
+            "animated": "magnezone.gif",
+            "sprite": "magnezone.png",
+            "icon": "magnezone-icon.png"
+        }
     }, {
         "name": "Marowak (Alolan)",
         "types": ["Fire", "Ghost"],
-        "images": [
-            "marowak_alola.gif",
-            "marowak_alola.png"
-        ],
-        "icon": "marowak_alola-icon.png"
+        "images": {
+            "animated": "marowak_alola.gif",
+            "sprite": "marowak_alola.png",
+            "icon": "marowak_alola-icon.png"
+        }
     }, {
         "name": "Dhelmise",
         "types": ["Grass", "Ghost"],
-        "images": [
-            "dhelmise.gif",
-            "dhelmise.png"
-        ],
-        "icon": "dhelmise-icon.png"
+        "images": {
+            "animated": "dhelmise.gif",
+            "sprite": "dhelmise.png",
+            "icon": "dhelmise-icon.png"
+        }
     }
 ];
 
